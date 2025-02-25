c14a8d413794b178fb35bc20ff6fb634c1227aaf
rodrigodssa <rodrigodssalmeida@gmail.com>
2025-02-25 15:12:52 -0300

    feat(cliente): adicionar classe Cliente e método para inicializar dados

---
+++ src/Desafio.java
@@ -0,0 +1,17 @@
+public class Desafio {
+    public static void main(String[] args) {
+
+        String nome = "João";
+        String tipoConta = "Conta Corrente";
+        double saldo = 15000.0;
+
+        System.out.println("*****************************");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("Tipo de conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("\n*****************************");
+
+
+
+    }
+}
