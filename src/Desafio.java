528c830a5fb587007b5c8676d617462db93e9a25
rodrigodssa <rodrigodssalmeida@gmail.com>
2025-02-25 15:32:00 -0300

    feat: adicionar Scanner para entrada de dados e loop while para interação

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Desafio {
     public static void main(String[] args) {
 
         String nome = "João";
         String tipoConta = "Conta Corrente";
         double saldo = 15000.0;
+        int opcao = 0;
+
 
         System.out.println("*****************************");
         System.out.println("\nNome do cliente: " + nome);
@@ -11,7 +15,20 @@ public class Desafio {
         System.out.println("Saldo atual: " + saldo);
         System.out.println("\n*****************************");
 
+        String menu = """
+                ** Digite sua opção **
+                1 - Consultar saldo
+                2 - Transferir valor
+                3 - Depositar valor
+                4 - Sair
+                """;
 
+        Scanner leitura = new Scanner(System.in);
 
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+
+        }
     }
 }
